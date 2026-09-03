133ee552fdfdd3fab9b321605b5301ddcf36675c
bsoft-johnsy <johnsymary1a@dbcyelagiri.edu.in>
2026-09-03 18:15:40 +0530

    Error fixed ProfileService

--- Source/backend/Service/CFR.AcutisService/ImplicitUsings.cs
+++ Source/backend/Service/CFR.AcutisService/ImplicitUsings.cs
@@ -20,3 +20,4 @@ global using CFR.DBEngine;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Hosting;
