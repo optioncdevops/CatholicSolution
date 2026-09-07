a471f6bc6615d0910c0c5736e21a9cca961add92
vijayaaron <vijayaaron@boscosofttech.com>
2026-09-07 09:39:40 +0530

    Changes

--- Source/frontend/CFR_Admin/src/App.tsx
+++ Source/frontend/CFR_Admin/src/App.tsx
@@ -13,7 +13,7 @@ import { emailTemplatesRoutes } from "@/modules/administration/emailTemplates";
 import { emailSettingsRoutes } from "@/modules/administration/emailSettings";
 import { organizationsRoutes } from "@/modules/organizations";
 import { requestsRoutes } from "@/modules/requests";
-import { productsRoutes } from "@/modules/products";
+import { productsRoutes } from "@/modules/Products";
 
 const DashboardPage = lazy(() =>
   import("@/modules/DashboardPage").then((m) => ({ default: m.DashboardPage })),
