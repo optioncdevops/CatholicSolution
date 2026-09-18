ed1bd9a3dbd2a14b1731ef336b50ec6a1876f1f5
vijayaaron <vijayaaron@boscosofttech.com>
2026-09-18 17:49:10 +0530

    Changes

--- Source/frontend/CFR_Admin/src/modules/administration/userRoles/pages/partials/UserRoleFormModal.tsx
+++ Source/frontend/CFR_Admin/src/modules/administration/userRoles/pages/partials/UserRoleFormModal.tsx
@@ -180,9 +180,9 @@ const UserRoleFormModal = ({ open, role, onClose, onSaved, readOnly = false }: U
         ) : null}
         <div className="flex flex-col gap-1">
           <InputField id="txtUserRoleName" control={control} name="roleName" label="Role name" required autoFocus rules={userRolesRules.roleName} maxLength={ROLE_NAME_MAX_LENGTH} disabled={saving || readOnly} />
-          <div className="flex justify-end">
+          {/* <div className="flex justify-end">
             <CharacterCount id="txtUserRoleName-counter" length={roleNameValue?.length ?? 0} maxLength={ROLE_NAME_MAX_LENGTH} />
-          </div>
+          </div> */}
         </div>
         <TextareaField control={control} name="description" label="Description" rows={3} rules={userRolesRules.description} maxLength={250} showCharCount={true} disabled={saving || readOnly} />
       </form>
